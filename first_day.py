e1b366bf91925a9542f8644118ba33bbfcc2c722
Anna Golomedova <golomedova@gmail.com>
2017-08-07 15:15:52 +0400

    update

--- first_day.py
+++ first_day.py
@@ -3,26 +3,27 @@ from copy import deepcopy
 from collections import defaultdict
 
 #invalid chars
-invChrs = 'ьъы'
+inv_chrs = 'ьъы'
 
 # returns last valid char from input string
-def GetLastChr(inputStr):
+def get_last_chr(inputstr):
     i = 1
-    while inputStr[-i] in invChrs and i < inputStr.__len__():
+    while inputstr[-i] in inv_chrs and i < len(inputstr):
         i = i + 1
-    lastChr = inputStr[-i]
-    if lastChr in invChrs:
-        lastChr = None
-    return lastChr
+    last_chr = inputstr[-i]
+    if last_chr in inv_chrs:
+        last_chr = None
+    return last_chr
 
 
 # returns first char for non-empty string
-def GetFirstChr(inputstr):
-    if inputstr.__len__() != 0:
-        firstchr = inputstr[0]
+def get_first_chr(inputstr):
+    if len(inputstr) != 0:
+        first_chr = inputstr[0]
     else:
-        firstchr = ""
-    return firstchr
+        first_chr = ""
+    return first_chr
+
 
 # importing txt file and making a dictionary
 with open('wg.txt') as slova:
@@ -30,58 +31,59 @@ with open('wg.txt') as slova:
     lines = [line.strip() for line in slova]
     for word in lines:
         words[word[0]].append(word)
-    print("words in dict \n")
+
 
 # try count
 count = 0
 
 # copy of a dictionary
-wordsСopy = deepcopy(words)
+words_copy = deepcopy(words)
+
 
 # bot new turn
-def BotTurn(player_word):
-    first_char = GetFirstChr(player_word)
-    if player_word in wordsСopy[first_char]:
-        del wordsСopy[first_char][wordsСopy[first_char].index(player_word)]
+def bot_turn(player_word):
+    first_char = get_first_chr(player_word)
+    if player_word in words_copy[first_char]:
+        del words_copy[first_char][words_copy[first_char].index(player_word)]
 
-    first_bot_letter = GetLastChr(player_word)
+    first_bot_letter = get_last_chr(player_word)
     print("So, my first letter is: ", first_bot_letter)
-    available_words_count = len(wordsСopy[first_bot_letter])
+    available_words_count = len(words_copy[first_bot_letter])
     if available_words_count == 0:
         return "Error"
     bot_choice_index = random.randrange(0, available_words_count)
-    available_words = wordsСopy[first_bot_letter]
+    available_words = words_copy[first_bot_letter]
     bot_choice = available_words[bot_choice_index]
 
-    del wordsСopy[first_bot_letter][bot_choice_index]
+    del words_copy[first_bot_letter][bot_choice_index]
     print("My new word is: ", bot_choice)
-    print("Your first letter is: ", GetLastChr(bot_choice))
-    return GetLastChr(bot_choice)
+    print("Your first letter is: ", get_last_chr(bot_choice))
+    return get_last_chr(bot_choice)
 
-firstTurn = True
+first_turn = True
 
 # player's turn
-def PlayerTurn(required_first_char):
-    global firstTurn
+def player_turn(required_first_char):
+    global first_turn
     while True:
         print("Print a word: ")
         player_word = str(input()).lower()
-        if firstTurn:
-            required_first_char =  GetFirstChr(player_word)
-        if ((player_word in wordsСopy[required_first_char]) or player_word == "stop"):
+        if first_turn:
+            required_first_char = get_first_chr(player_word)
+        if ((player_word in words_copy[required_first_char]) or player_word == "stop"):
             break
         else:
             print("Invalid first char or already used word, try another word: ")
-    firstTurn = False
+    first_turn = False
     return player_word
 
 first_required_char = ""
-print("Welcome here! To stop print \"stop\". Enjoy this game!")
+print("Welcome here! To stop print \"stop\". Enjoy this game!\n")
 while True:
-    new_player_word = PlayerTurn(first_required_char)
-    count+=1
-    if (new_player_word != "stop"):
-        first_required_char = BotTurn(new_player_word)
+    new_player_word = player_turn(first_required_char)
+    count += 1
+    if new_player_word != "stop":
+        first_required_char = bot_turn(new_player_word)
         if first_required_char == "Error":
             print("You're winner! Your result is ", count, " words!")
             break
